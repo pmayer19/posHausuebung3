021503caa6f2e984ba35dc79823c18447bd7f0db
Paul <mayerp190093@sus.htl-grieskirchen.at>
2021-10-10 21:21:20 +0200

    thirdCommit

--- PosHausuebung3/src/net/htlgkr/mayerp190093/PosHausuebung3/Main.java
+++ PosHausuebung3/src/net/htlgkr/mayerp190093/PosHausuebung3/Main.java
@@ -5,13 +5,32 @@
  */
 package net.htlgkr.mayerp190093.PosHausuebung3;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  *
  * @author mayer
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner fileScanner = new Scanner(new File("weapons.csv"));
+        List<Weapons> weapons = new ArrayList<>();
+        String line = fileScanner.nextLine();
+        while (fileScanner.hasNextLine()) {
+            line = fileScanner.nextLine();
+            String[] parts = line.split(";");
+            Weapons w = new Weapons(parts[0], parts[1], parts[2], Integer.parseInt(parts[3]), Integer.parseInt(parts[4]), Integer.parseInt(parts[4]), Integer.parseInt(parts[5]));
+            weapons.add(w);
+        }
+        fileScanner.close();
 
+        for (int i = 0; i < weapons.size(); i++) {
+            System.out.println(weapons.get(i));
+        }
     }
 }
