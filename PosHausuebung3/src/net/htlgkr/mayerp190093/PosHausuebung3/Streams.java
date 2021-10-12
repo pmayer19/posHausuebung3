86b172cc58801dc7bc18a7b50edf136041a97ae0
Paul <mayerp190093@sus.htl-grieskirchen.at>
2021-10-12 15:01:50 +0200

    sixthCommit

--- PosHausuebung3/src/net/htlgkr/mayerp190093/PosHausuebung3/Streams.java
+++ PosHausuebung3/src/net/htlgkr/mayerp190093/PosHausuebung3/Streams.java
@@ -2,6 +2,7 @@ package net.htlgkr.mayerp190093.PosHausuebung3;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.OptionalDouble;
@@ -28,45 +29,46 @@ public class Streams {
 
     }
 
-//    public Weapons findWeaponWithLowestDamage(List<Weapons> weapons) {
-//
-//    }
-//    public Weapons findWeaponWithHighestStrength(List<Weapons> weapons) {
-//        List<Integer> myList = new LinkedList<>();
-//        for (int i = 0; i < weapons.size(); i++) {
-//            myList.add(weapons.get(i).getDamage());
-//        }
-//
-//    }
-    //    public List<Weapons> collectMissileWeapons(List<Weapons> weapons) {
-    //        //implement this
-    //    }
-    //
-    //    public Weapons findWeaponWithLongestName(List<Weapons> weapons) {
-    //        //implement this
-    //    }
-    //
-    //    public List<String> toNameList(List<Weapons> weapons) {
-    //        //implement this
-    //    }
-    //
-    //    public int[] toSpeedArray(List<Weapons> weapons) {
-    //        //implement this
-    //    }
-    //
-    //    public int sumUpValues(List<Weapons> weapons) {
-    //        //implement this
-    //    }
-    //
-    //    public long sumUpHashCodes(List<Weapons> weapons) {
-    //        //implement this
-    //    }
-    //
-    //    public List<Weapons> removeDuplicates(List<Weapons> weapons) {
-    //        //implement this
-    //    }
-    //
-    //    public void increaseValuesByTenPercent(List<Weapons> weapons) {
-    //        //implement this
-    //    }
+    public Weapons findWeaponWithLowestDamage(List<Weapons> weapons) {
+        Weapons we = weapons.stream().min(Comparator.comparingInt(w -> w.getDamage())).orElse(null);
+        return we;
+    }
+
+    public Weapons findWeaponWithHighestStrength(List<Weapons> weapons) {
+        Weapons we = weapons.stream().max(Comparator.comparingInt(w -> w.getDamage())).orElse(null);
+        return we;
+    }
+
+    public List<Weapons> collectMissileWeapons(List<Weapons> weapons) {
+        return weapons.stream().filter(s -> s.getDamageType() == DamageType.MISSILE).collect(Collectors.toList());
+    }
+
+    public Weapons findWeaponWithLongestName(List<Weapons> weapons) {
+        Weapons we = weapons.stream().max(Comparator.comparingInt(s -> s.getName().length())).orElse(null);
+        return we;
+    }
+
+    public List<String> toNameList(List<Weapons> weapons) {
+        return (List<String>) weapons.stream().map(Weapons::getName);
+    }
+
+    public int[] toSpeedArray(List<Weapons> weapons) {
+        return weapons.stream().mapToInt(Weapons::getSpeed).toArray();
+    }
+
+    public int sumUpValues(List<Weapons> weapons) {
+        return weapons.stream().mapToInt(Weapons::getValue).sum();
+    }
+
+    public long sumUpHashCodes(List<Weapons> weapons) {
+        return weapons.stream().mapToInt(Weapons::hashCode).sum();
+    }
+
+    public List<Weapons> removeDuplicates(List<Weapons> weapons) {
+        return (List<Weapons>) weapons.stream().map(Weapons::getName).distinct();
+    }
+
+    public void increaseValuesByTenPercent(List<Weapons> weapons) {
+        weapons.stream().forEach(w -> w.setValue((int) (w.getValue() * 1.1)));
+    }
 }
